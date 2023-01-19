984609dd599e86fb24e68e64f0e5434b4fec89d8
budy6991 <alvmenfer@gmail.com>
2023-01-19 19:04:03 +0100

    Created user schema

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  membership: {
+    type: String,
+    required: true,
+    enum: ["member", "admin", "user"],
+  },
+});
+
+UserSchema.virtual("fullname").get(function () {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
